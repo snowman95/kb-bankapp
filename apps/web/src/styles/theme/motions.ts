c957f74085691b6cd34ebdc7c533b11de53e78db
snowman95 <hun2ya9@gmail.com>
2022-10-18 21:03:56 +0900

    애니메이션 적용을 위한 framer motion 컴포넌트 추가하라

---
+++ apps/web/src/styles/theme/motions.ts
@@ -0,0 +1,39 @@
+import styled from "@emotion/styled";
+import { motion } from "framer-motion";
+
+export type TRepeatType = "loop" | "reverse" | "mirror" | "undefined";
+export const variants = {
+  fadeIn: {
+    initial: { opacity: 0 },
+    animate: { opacity: 1 },
+  },
+};
+export const AnimatedFrame = styled(motion.div)`
+  width: 100%;
+  height: 100%;
+`;
+AnimatedFrame.defaultProps = {
+  variants: variants.fadeIn,
+  initial: "initial",
+  animate: "animate",
+};
+
+export const AnimatedFullFrame = styled(motion.div)`
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: 1000;
+
+  background: rgba(51, 61, 74, 0.5);
+`;
+AnimatedFullFrame.defaultProps = {
+  variants: variants.fadeIn,
+  initial: "initial",
+  animate: "animate",
+};
