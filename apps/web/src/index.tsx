5ba7d3597f28fcfca6089e577e36cce366245116
snowman95 <hun2ya9@gmail.com>
2022-10-18 21:06:01 +0900

    index 파일에 Redux Store, theme, Router 추가하라

--- apps/web/src/index.tsx
+++ apps/web/src/index.tsx
@@ -1,5 +1,19 @@
+import { ThemeProvider } from "@emotion/react";
+import store from "modules";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import theme from "styles/theme";
 import App from "./App";
 import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
+  </Provider>,
+  document.getElementById("root")
+);
