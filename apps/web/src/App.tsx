d3f04d92d4032d9e2bf223b567bc09f83dd1b648
snowman95 <hun2ya9@gmail.com>
2022-10-18 21:06:48 +0900

    App 컴포넌트에 기본적으로 필요한 세팅 진행하라

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,7 +1,31 @@
+/** @jsxImportSource @emotion/react */
 import React from "react";
+import { AnimatePresence } from "framer-motion";
+import { ResponsiveWrapper, 중앙정렬용ContainerStyle } from "styles";
+import { AnimatedFrame } from "styles/theme/motions";
+import { useTypedSelector } from "lib/hooks/useStore";
+import { Route, Routes, useLocation } from "react-router-dom";
+import { useDeviceResizer } from "lib/hooks/useDeviceResizer";
 
 function App() {
-  return <>hello world!</>;
+  const location = useLocation();
+  useDeviceResizer();
+  const isMobileView = useTypedSelector(
+    (state) => state.settingStore.isMobileView
+  );
+
+  return (
+    <AnimatePresence exitBeforeEnter initial={false}>
+      <AnimatedFrame css={중앙정렬용ContainerStyle}>
+        <ResponsiveWrapper isMobileView={isMobileView}>
+          <Routes key={location.key} location={location}>
+            <Route path="/" element={<>home page</>} />
+            <Route path="*" element={<div>error page</div>}></Route>
+          </Routes>
+        </ResponsiveWrapper>
+      </AnimatedFrame>
+    </AnimatePresence>
+  );
 }
 
 export default App;
