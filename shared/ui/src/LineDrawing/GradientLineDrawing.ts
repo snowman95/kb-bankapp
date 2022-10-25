e6515b8e44d0b48ba0c5973b7e7a0fc8e44f349e
snowman95 <hun2ya9@gmail.com>
2022-10-25 21:42:27 +0900

    GradientLineDrawing 컴포넌트를 제거하고 함수를 반환하도록 변경하라

--- shared/ui/src/LineDrawing/GradientLineDrawing.ts
+++ shared/ui/src/LineDrawing/GradientLineDrawing.ts
@@ -1,9 +1,29 @@
 import { GradientPath } from 'gradient-path'
-import { useEffect, useRef, useState } from 'react'
 import { animate } from 'framer-motion'
-import { StarSvg } from './Path'
+
 const GAP = 0.01
 const GAP_MUL = 10
+
+interface GP {
+    render({
+        type,
+        fill,
+        width
+    }: {
+        type: 'path' | 'circle'
+        fill: { color: string; pos: number }[]
+        width: number
+    }): void
+}
+
+interface setPathAnimationProps {
+    gp: GP
+    duration: number
+    lineWidth: number
+    playReverse: boolean
+    mainColor: string
+    pointColor: string
+}
 
 const getGradientColor = ({
     v,
@@ -67,68 +87,39 @@ const reset = () => {
     const elements = document.querySelectorAll('.element-path')
     elements.forEach(element => element.remove())
 }
-
-interface GradientLineDrawingProps {
-    duration: number
-    lineWidth: number
-    playReverse: boolean
-    mainColor: string
-    pointColor: string
+export const createGradientPath = (
+    pathRef: React.RefObject<SVGPathElement>
+) => {
+    return new GradientPath({
+        path: pathRef?.current,
+        segments: 80, // 얼마나 많은 다른 색상을 표시?
+        samples: 10, //  샘플이 많을수록 경로가 더 구체적으로 둥글게 됩니다.
+        precision: 2 // Optional 경로의 각 점에 대해 유지할 소수 자릿수
+    })
 }
-
-interface GP {
-    render({
-        type,
-        fill,
-        width
-    }: {
-        type: 'path' | 'circle'
-        fill: { color: string; pos: number }[]
-        width: number
-    }): void
-}
-
-export const GradientLineDrawing = ({
+export const setPathAnimation = ({
+    gp,
     duration,
     lineWidth,
     playReverse,
     mainColor,
     pointColor
-}: GradientLineDrawingProps) => {
-    const pathRef = useRef<SVGPathElement>(null)
-    const [gp, setGp] = useState<GP>()
-    useEffect(() => {
-        if (!gp) {
-            if (!pathRef) return
-            const newGp = new GradientPath({
-                path: pathRef?.current,
-                segments: 80, // 얼마나 많은 다른 색상을 표시?
-                samples: 10, //  샘플이 많을수록 경로가 더 구체적으로 둥글게 됩니다.
-                precision: 2 // Optional 경로의 각 점에 대해 유지할 소수 자릿수
+}: setPathAnimationProps) => {
+    return animate(0, 1, {
+        duration,
+        onPlay: () => reset(),
+        onRepeat: () => reset(),
+        onUpdate: v => {
+            gp.render({
+                type: 'path',
+                fill: getGradientColor({
+                    v,
+                    playReverse,
+                    mainColor,
+                    pointColor
+                }),
+                width: lineWidth
             })
-            setGp(newGp)
         }
-    }, [])
-    useEffect(() => {
-        if (!gp) return
-        const controls = animate(0, 1, {
-            duration,
-            onPlay: () => reset(),
-            onRepeat: () => reset(),
-            onUpdate: v => {
-                gp.render({
-                    type: 'path',
-                    fill: getGradientColor({
-                        v,
-                        playReverse,
-                        mainColor,
-                        pointColor
-                    }),
-                    width: lineWidth
-                })
-            }
-        })
-        return controls.stop
     })
-    return <StarSvg ref={pathRef} />
 }
