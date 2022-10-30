1aa521031d12de20f243eaad80e1c4b93cf5ff58
snowman95 <hun2ya9@gmail.com>
2022-10-30 12:46:41 +0900

    web 패키지가 루트경로를 포함하도록 변경하라

--- apps/web/craco.config.js
+++ apps/web/craco.config.js
@@ -1,4 +1,10 @@
 const CracoAlias = require("craco-alias");
+const fs = require("fs");
+const path = require("path");
+const cracoBabelLoader = require("craco-babel-loader");
+
+const appDirectory = fs.realpathSync(process.cwd());
+const resolveApp = (relativePath) => path.resolve(appDirectory, relativePath);
 
 module.exports = {
   plugins: [
@@ -7,6 +13,12 @@ module.exports = {
       options: {
         source: "tsconfig",
         tsConfigPath: "tsconfig.paths.json",
+      },
+    },
+    {
+      plugin: cracoBabelLoader,
+      options: {
+        includes: [resolveApp("../apps")],
       },
     },
   ],
