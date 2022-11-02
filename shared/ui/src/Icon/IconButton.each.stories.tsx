f9bfca4b5d7a50db9a3a7c50c11e1b54d58e5a79
snowman95 <hun2ya9@gmail.com>
2022-11-02 13:38:20 +0900

    IconButton 컴포넌트를 통합 버전으로 업그레이드 하고 each, template 스토리 만들어라

--- shared/ui/src/Icon/IconButton.each.stories.tsx
+++ shared/ui/src/Icon/IconButton.each.stories.tsx
@@ -1,11 +1,11 @@
 /** @jsx jsx */
+import { jsx } from '@emotion/react'
+import * as Icon from '../assets/svgr'
 import IconButton from './IconButton'
 import { ComponentStory, ComponentMeta } from '@storybook/react'
-import { jsx, css } from '@emotion/react'
-import * as Icon from '../assets/svgr'
 
 export default {
-    title: 'components/Icon', // | 안 쓰도록 조심
+    title: 'components/Icon/Button/Each', // | 안 쓰도록 조심
     component: IconButton
 }
 const Template: ComponentStory<typeof IconButton> = args => (
@@ -13,57 +13,118 @@ const Template: ComponentStory<typeof IconButton> = args => (
 )
 const templateIcon = Template.bind({})
 templateIcon.args = {
-    rotationDir: 'LEFT',
     disabled: false,
-    onclick: () => console.log('Icon clicked')
+    onclick: () => console.log('icon button clicked')
 }
+
 export const HeaderArrow = Template.bind({})
 HeaderArrow.args = {
     ...templateIcon.args,
+    theme: 'HEADER',
     icon: <Icon.Header.Arrow />
 }
 export const HeaderHome = Template.bind({})
 HeaderHome.args = {
     ...templateIcon.args,
+    theme: 'HEADER',
     icon: <Icon.Header.Home />
 }
 export const HeaderMenu = Template.bind({})
 HeaderMenu.args = {
     ...templateIcon.args,
+    theme: 'HEADER',
     icon: <Icon.Header.Menu />
 }
+export const HeaderAlert = Template.bind({})
+HeaderAlert.args = {
+    ...templateIcon.args,
+    theme: 'HEADER',
+    icon: <Icon.Header.Alert />
+}
+export const HeaderSearch = Template.bind({})
+HeaderSearch.args = {
+    ...templateIcon.args,
+    theme: 'HEADER',
+    icon: <Icon.Header.Search />
+}
+
 export const CardArrow = Template.bind({})
 CardArrow.args = {
     ...templateIcon.args,
+    theme: 'PAY',
     icon: <Icon.Pay.CardArrow />
 }
 export const CardRegistration = Template.bind({})
 CardRegistration.args = {
     ...templateIcon.args,
+    theme: 'PAY',
     icon: <Icon.Pay.CardRegistration />
 }
 export const Membership = Template.bind({})
 Membership.args = {
     ...templateIcon.args,
+    theme: 'PAY',
     icon: <Icon.Pay.Membership />
 }
 export const OnSite = Template.bind({})
 OnSite.args = {
     ...templateIcon.args,
+    theme: 'PAY',
     icon: <Icon.Pay.OnSite />
 }
 export const QrCode = Template.bind({})
 QrCode.args = {
     ...templateIcon.args,
+    theme: 'PAY',
     icon: <Icon.Pay.QrCode />
 }
 export const TransferArrow = Template.bind({})
 TransferArrow.args = {
     ...templateIcon.args,
+    theme: 'PAY',
     icon: <Icon.Pay.TransferArrow />
 }
 export const Vibration = Template.bind({})
 Vibration.args = {
     ...templateIcon.args,
+    theme: 'PAY',
     icon: <Icon.Pay.Vibration />
+}
+
+export const KbpickAccount = Template.bind({})
+KbpickAccount.args = {
+    ...templateIcon.args,
+    theme: 'KBPICK',
+    icon: <Icon.Kbpick.Account />
+}
+export const KbpickCar = Template.bind({})
+KbpickCar.args = {
+    ...templateIcon.args,
+    theme: 'KBPICK',
+    icon: <Icon.Kbpick.Car />
+}
+export const KbpickChat = Template.bind({})
+KbpickChat.args = {
+    ...templateIcon.args,
+    theme: 'KBPICK',
+    icon: <Icon.Kbpick.Chat />
+}
+export const KbpickFinancialProducts = Template.bind({})
+KbpickFinancialProducts.args = {
+    ...templateIcon.args,
+    theme: 'KBPICK',
+    icon: <Icon.Kbpick.FinancialProducts />
+}
+export const KbpickMydata = Template.bind({})
+KbpickMydata.args = {
+    ...templateIcon.args,
+    theme: 'KBPICK',
+    icon: <Icon.Kbpick.Mydata />
+}
+
+export const Star = Template.bind({})
+Star.args = {
+    ...templateIcon.args,
+    theme: 'STAR',
+    icon: <Icon.Star.Star />
 }
