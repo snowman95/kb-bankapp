89b07e35a1d1ebe805f68dedf1a2f4216f9b1d1f
snowman95 <hun2ya9@gmail.com>
2022-11-05 00:14:30 +0900

    Service 컴포넌트 title에 개행 추가하라

--- apps/web/src/components/Asset/Service/index.tsx
+++ apps/web/src/components/Asset/Service/index.tsx
@@ -19,17 +19,17 @@ interface ServiceProps {
 
 const TYPE = {
   경제상식: {
-    title: "재테크 고수가 되기 위한 필수 경제상식",
+    title: `재테크 고수가 되기 위한 필수\n경제상식`,
     icon: EconomyIcon,
     hashtags: ["돈되는 상식", "금융꿀팁"],
   },
   보험: {
-    title: "[보험] 위험 대비 상품, 가장 많이 판매된 베스트",
+    title: `[보험] 위험 대비 상품, 가장\n많이 판매된 베스트`,
     icon: ChartIcon,
     hashtags: ["글로벌 금융시장 동향", "주요지수"],
   },
   주식리포트: {
-    title: "일주일을 마감하는 주간시장 리포트",
+    title: `일주일을 마감하는 주간시장\n리포트`,
     icon: ChartIcon,
     hashtags: ["글로벌 금융시장 동향", "주요지수"],
   },
